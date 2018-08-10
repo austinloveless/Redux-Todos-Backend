6682150ce01587dd591da7af40356f72df71bd0f
Austin Loveless <35633324+austinloveless@users.noreply.github.com>
2018-08-09 18:55:56 -0600

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb://bill5171:bill5171@ds235860.mlab.com:35860/redux_todo"
-);
+
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
 
